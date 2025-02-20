c1c20082cb2c76496a9796d47f0c53f6a3e08485
Göktuğ Murat Demir <goktugmuratdemir@tcddteknik.com.tr>
2025-02-20 10:13:22 +0300

    natiwind tailwind.css added

--- app/index.tsx
+++ app/index.tsx
@@ -5,9 +5,16 @@ const HomeScreen = () => {
   const router = useRouter();
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 24, fontWeight: "bold" }}>🏠 Home Page</Text>
-      <Button title="Go to Users" onPress={() => router.push("../users")} />
+    <View className="flex justify-center items-center h-full gap-8">
+      <View className="flex justify-center items-center gap-2">
+        <Text className="text-2xl font-bold">🏠 Home Page</Text>
+        <Button title="Go to Users" onPress={() => router.push("../users")} />
+      </View>
+      <View>
+        <Text className="text-red-600 text-sm">
+          This is a simple example of how to use the expo-router package.
+        </Text>
+      </View>
     </View>
   );
 };
